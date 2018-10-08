2e7db24f64661c8cf9cf5990eb608024fdd11eee
RnD009 <43023599+RnD009@users.noreply.github.com>
2018-10-08 14:19:33 +0700

    Add files via upload

--- arduino/pressure_sensor/pressure_sensor.ino
+++ arduino/pressure_sensor/pressure_sensor.ino
@@ -1,70 +1,39 @@
-#include <LiquidCrystal_I2C.h>
-#include <LiquidCrystal.h>
-#include <Wire.h> 
-
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
-
-float senVal1 = analogRead(A0);
-float senVal2 = analogRead(A1);
-float senVal3 = analogRead(A2);
-float senVal4 = analogRead(A3);
+///////////////////////////////////////////////////////
+float senVal1;
+float senVal2;
+float senVal3;
+float senVal4;
+float senVal_sw;
 
 float voltage;
 float pressure_pascal;
 float pressure_bar;
 float pressure_psi;
 
-
-//int senVal_sw = analogRead(A5);
-
 void setup() {
   // put your setup code here, to run once:
       Serial.begin(38400);
-      lcd.begin();
-      pinMode(senVal1, INPUT);
-      pinMode(senVal2, INPUT);
-      pinMode(senVal3, INPUT);
-      pinMode(senVal4, INPUT);
-      //pinMode(senVal_sw, INPUT);
-      
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Sensor Value: ");
-  Serial.print(senVal1);
 
+  senVal1 = analogRead(A0);
+  senVal2 = analogRead(A1);
+  senVal3 = analogRead(A2);
+  senVal4 = analogRead(A3);
+  senVal_sw = analogRead(A4);
+  
+  // voltage
   voltage = (senVal1 * 5.0) / 1024.0;
-    Serial.print("Volts: ");
-    Serial.print(voltage);
-   
-  pressure_pascal = (3.0*((float)voltage-0.47))*1000000.0;
-  pressure_bar = pressure_pascal/10e5;
-    Serial.print("Pressure = ");
-    Serial.print(pressure_bar);
-    Serial.println(" bars");
-
+  // pascal
+  pressure_pascal = (3.0*((float)voltage-0.47))*100000.0;
+  //bar
+  pressure_bar = pressure_pascal/1e5;
+  //psi
   pressure_psi =  pressure_bar * 14.5038;
-    Serial.print("Pressure = ");
-    Serial.print(pressure_psi);
-    Serial.println(" psi");
+  Serial.print(pressure_psi);
+  Serial.println(" Psi");
+  delay(100);
 
-// psi
-    lcd.setCursor(0,0);
-    lcd.print(pressure_psi);
-    lcd.setCursor(11,0);
-    lcd.print(" Psi");
-
-// bar
-    lcd.setCursor(0,1);
-    lcd.print(pressure_bar);
-    lcd.setCursor(11,1);
-    lcd.print(" bar");
-
-
-  if  (pressure_psi >= 120.0){
-    lcd.setCursor(0,1);
-    lcd.print("COMPRESSOR OFF");
-    }
-    
 }
